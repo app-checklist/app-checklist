73c4c26211eb9fb43efe4a5122474c48183c9c5e
Niclas van Eyk <niclas.eyk@gmail.com>
2021-06-05 20:03:59 +0200

    Added custom demo component

---
+++ components/Demo.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Demo() {
+    return <div style={{ background: 'hotpink' }}>
+        <h1>I am a beautiful <i>custom</i> component!</h1>
+    </div>
+}
